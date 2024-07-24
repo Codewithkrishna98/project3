3831c98f8e36fcb83ef89a95b2926155294245bb
Codewithkrishna98 <143336373+Codewithkrishna98@users.noreply.github.com>
2024-07-24 11:51:24 +0530

    remov border

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-       <div className=" p-5  flex justify-center  border text-2xl font-bold ">
+       <div className=" p-5  flex justify-center  text-2xl font-bold ">
         <div className=" mt-40 border w-fit p-4 rounded-md bg-gray-500 hover:bg-slate-400 cursor-pointer  shadow-md">
 
         <Link href={"/user-management"}>
